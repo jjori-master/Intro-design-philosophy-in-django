ca97eb9a8cd7ac82dd8cc09807d077db41b629b2
jjori <jjori.master@gmail.com>
2022-08-30 21:28:54 +0900

    login/logout 구현 및 login_required 장식자를 사용한 인증

--- app/views.py
+++ app/views.py
@@ -1,14 +1,21 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import CreateView
 
 from app.forms import PostForm
 from app.models import Post
 
+
 # Create your views here.
 
 
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
+    # login required 장식자를 사용함으로 아래 로직을 사용하지 않아도 된다.
+    # if not request.user.is_authenticated:
+    #     return redirect("/accounts/login")
+
     qs = Post.objects.all()
     return render(request, "app/index.html", {"post_list": qs})
 
